3001a7da3118ab82dd7ab73329a1a3f29f9e9579
钟浩 <zh826256645@gmail.com>
2020-04-22 20:51:59 +0800

    1. 增加 README.md 的说明 2. 修改变量的命令 3. 增加链表的快速排序

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# my-algorithm-exercises
-算法练习
+# [my-algorithm-exercises](https://github.com/zh826256645/my-algorithm-exercises)
+![language](https://img.shields.io/badge/language-python-blue.svg)
+
+深入了解数据结构和算法练习
+
+## 运行
+1. 本代码在 python3.7 的环境下进行编写
+
+2. 执行代码前请将本项目的根目录加入 `PYTHONPATH` 中, 例如项目在根目录下 `/my-algorithm-exercises`
+    ```shell script
+    export PYTHONPATH=$PYTHONPATH:/my-algorithm-exercises
+    ```
+    如果是在 vscode 下进行调试，可以在调试文件中加入如下配置
+    ```
+    "env": {
+        "PYTHONPATH": "$PYTHONPATH:${workspaceRoot}"
+    }
+    ```
+
+## 项目
+1. [链表](https://github.com/zh826256645/my-algorithm-exercises/blob/master/model/link.py)
+    - [x] [计算链表中环的长度](https://github.com/zh826256645/my-algorithm-exercises/blob/b43234d586074a9da4a432893b38d1729c157b1d/exercises/link/link.py#L62)
+    - [x] [对无序链表进行插入排序](https://github.com/zh826256645/my-algorithm-exercises/blob/b43234d586074a9da4a432893b38d1729c157b1d/exercises/link/link_sort.py#L74)
+    - [x] [对无序链表进行选择排序](https://github.com/zh826256645/my-algorithm-exercises/blob/b43234d586074a9da4a432893b38d1729c157b1d/exercises/link/link_sort.py#L109)
+    - [] 对链表进行快速排序
--- exercises/link/link_sort.py
+++ exercises/link/link_sort.py
@@ -84,22 +84,22 @@ def optimize_insertion_sort(link: Link) -> Link:
         执行用时: 220 ms, 在所有 Python3 提交中击败了 74.71% 的用户
         内存消耗: 15.3 MB, 在所有 Python3 提交中击败了 12.50% 的用户
     """
-    current_node = link.head_node
     head_node = Node(0, link.head_node)
 
+    current_node = link.head_node
     while current_node and current_node.next_node:
         if current_node <= current_node.next_node:
             current_node = current_node.next_node
             continue
 
-        head_current_node = head_node
-        while head_current_node.next_node < current_node.next_node:
-            head_current_node = head_current_node.next_node
+        start_node = head_node
+        while start_node.next_node < current_node.next_node:
+            start_node = start_node.next_node
 
-        _current_node = current_node.next_node
-        current_node.next_node = _current_node.next_node
-        _current_node.next_node = head_current_node.next_node
-        head_current_node.next_node = _current_node
+        temp_node = current_node.next_node
+        current_node.next_node = temp_node.next_node
+        temp_node.next_node = start_node.next_node
+        start_node.next_node = temp_node
 
     link.head_node = head_node.next_node
     return link
@@ -110,36 +110,44 @@ def selection_sort(link: Link) -> Link:
     """
     选择排序
 
+    说明:
+        1. 交换任意两个节点，需要用定位到它们的父节点
+        2. 要分相邻和不相邻两种情况处理
+
     测试:
         链表的长度为 1000 --- Total execution time: 656 ms
         链表的长度为 10000 --- Total execution time: 67671 ms
     """
     head_node = Node(0, link.head_node)
+
     start_node = head_node
     while start_node.next_node:
-        swap_last_node = None
+        parent_node = None
+
         current_node = start_node.next_node
         while current_node.next_node:
             if current_node.next_node < start_node.next_node:
-                if not swap_last_node or swap_last_node.next_node > current_node.next_node:
-                    swap_last_node = current_node
+                if not parent_node or parent_node.next_node > current_node.next_node:
+                    parent_node = current_node
 
             current_node = current_node.next_node
 
-        if swap_last_node:
-            if swap_last_node.next_node == start_node.next_node.next_node:
+        if parent_node:
+            if parent_node.next_node == start_node.next_node.next_node:
                 # 相邻
-                swap_node = swap_last_node.next_node
+                swap_node = parent_node.next_node
                 start_node.next_node.next_node = swap_node.next_node
                 swap_node.next_node = start_node.next_node
                 start_node.next_node = swap_node
             else:
                 # 不相邻
-                swap_node = swap_last_node.next_node
-                swap_start_node = start_node.next_node
-                swap_node.next_node, swap_start_node.next_node = swap_start_node.next_node, swap_node.next_node
-                start_node.next_node = swap_node
-                swap_last_node.next_node = swap_start_node
+                swap_node_a = start_node.next_node
+                swap_node_b = parent_node.next_node
+
+                swap_node_b.next_node, swap_node_a.next_node = swap_node_a.next_node, swap_node_b.next_node
+
+                start_node.next_node = swap_node_b
+                parent_node.next_node = swap_node_a
 
         start_node = start_node.next_node
 
@@ -176,34 +184,65 @@ def selection_sort_swap_value(link: Link) -> Link:
 
 
 @measure
-def quicksort(link: Link) -> Link:
+def quicksort_main(link: Link) -> Link:
     """
     快速排序（递归）
+
+    测试:
+        链表的长度为 1000 --- Total execution time: 11 ms
+        链表的长度为 10000 --- Total execution time: 143 ms
+
+    问题:
+        1. 链表太长时，递归深度太大，影响性能
+
+    优化:
+        1. 不使用递归的方式
     """
-    pass
+    if link.head_node and link.head_node.next_node:
+        quicksort(link.head_node, Node)
+    return link
 
 
-def partition_link(low_node: Node, height_node: Node) -> Node:
+def quicksort(head_node: Node, tail_node: Node):
     """
-    将链表进行分区
+    快速排序（递归）
+
+    说明：
+        右链表右边界使用闭区间，左链表的左边界使用开区间
+    """
+    if head_node and head_node != tail_node and head_node.next_node != tail_node:
+        middle_node = partition_link(head_node, tail_node)
+        quicksort(head_node, middle_node)
+        quicksort(middle_node.next_node, tail_node)
+
+
+def partition_link(start_node: Node, end_node: Node) -> Node:
+    """
+    将这段链表进行分区
+
+    使用交换值的方式
+    :Return 这段链表的中间节点
     """
     # 取最开头的节点为中心点
-    pivot = low_node.element
+    pivot = start_node.element
 
-    loc_node = low_node
-    current_node = low_node.next_node
-    while current_node and current_node != height_node:
+    # 计算中间节点的位置
+    middle_node = start_node
+
+    current_node = start_node.next_node
+    while current_node and current_node != end_node:
         if current_node.element < pivot:
-            loc_node = loc_node.next_node
-            current_node.element, loc_node.element = loc_node.element, current_node.element
+            middle_node = middle_node.next_node
+            current_node.element, middle_node.element = middle_node.element, current_node.element
+
         current_node = current_node.next_node
-    low_node.element, loc_node.element = loc_node.element, low_node.element
-    return loc_node
+    start_node.element, middle_node.element = middle_node.element, start_node.element
+    return middle_node
 
 
 if __name__ == "__main__":
-    link = init_link(1000, True)
+    link = init_link(10000, True)
     # print(link)
     # link = insertion_sorting(link)
-    link = selection_sort(link)
+    link = quicksort_main(link)
     # print(link)
