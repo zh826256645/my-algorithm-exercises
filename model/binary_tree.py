1a7610c360544839493957b423d8887a09f6b4e2
钟浩 <zh826256645@gmail.com>
2020-08-17 09:56:18 +0800

    feat(二叉树): 新增笛卡尔树

--- model/binary_tree.py
+++ model/binary_tree.py
@@ -54,14 +54,12 @@ class BiTNode:
         return f'BiTNode[{self.element}]'
 
 
-class BinarySearchTree:
-    """二叉搜索树
+class BinaryTree:
+    """二叉树
 
-    满足以下条件
-        1. 若任意节点的左子树不空，则左子树上所有节点的值均小于它的根节点的值；
-        2. 若任意节点的右子树不空，则右子树上所有节点的值均大于或等于它的根节点的值；
-        3. 任意节点的左、右子树也分别为二叉查找树；
-
+    Keyword arguments:
+    argument -- description
+    Return: return_description
     """
 
     def __init__(self, root=None) -> None:
@@ -69,63 +67,16 @@ class BinarySearchTree:
 
         Keyword arguments:
         root -- 根节点
-        Return: return_description
         """
 
         self.root = root
-
-    def put(self, element) -> bool:
-        """插入节点
-
-        Keyword arguments:
-        element -- 节点值
-        Return: return_description
-        """
-        node = BiTNode(element)
-        if not self.root:
-            self.root = node
-        else:
-            current = self.root
-            while current:
-                if node < current:
-                    if not current.left:
-                        current.left = node
-                        break
-                    else:
-                        current = current.left
-                else:
-                    if not current.right:
-                        current.right = node
-                        break
-                    else:
-                        current = current.right
-
-    def get(self, element, node=None):
-        """查找第一个匹配元素的节点
-
-        Keyword arguments:
-        element -- 匹配的元素
-        node -- 开始的节点
-        Return: return_description
-        """
-        current = node
-        if not current:
-            current = self.root
-
-        while current:
-            if current.element == element:
-                return current
-            elif current.element > element:
-                current = current.left
-            else:
-                current = current.right
 
     def pre_order(self, node) -> str:
         """前序遍历
 
         Keyword arguments:
         node -- 节点
-        Return: return_description
+        Return: 遍历结果
         """
         _str = ''
         if node:
@@ -139,7 +90,7 @@ class BinarySearchTree:
 
         Keyword arguments:
         node -- 节点
-        Return: return_description
+        Return: 遍历结果
         """
         _str = ''
         if node:
@@ -153,7 +104,7 @@ class BinarySearchTree:
 
         Keyword arguments:
         node -- 节点
-        Return: return_description
+        Return: 遍历结果
         """
         _str = ''
         if node:
@@ -167,7 +118,7 @@ class BinarySearchTree:
 
         Keyword arguments:
         node -- 节点
-        Return: return_description
+        Return: 遍历结果
         """
         _str = ''
 
@@ -189,6 +140,167 @@ class BinarySearchTree:
             lever = last_lever
 
         return _str
+
+    @staticmethod
+    def has_child(node) -> bool:
+        """判断节点是否有子节点
+
+        Keyword arguments:
+        node -- 节点
+        Return: true or false
+        """
+        if node.right or node.left:
+            return True
+        return False
+
+    def format_out(self, node=None):
+        """格式化输出
+
+        Keyword arguments:
+        node -- 节点
+        """
+        if not node:
+            node = self.root
+
+        print(node)
+        self._format_out(node)
+
+    def _format_out(self, node, prefix=None):
+        if not prefix:
+            prefix = ''
+        else:
+            prefix = prefix.replace(PREFIX_BRANCH, PREFIX_TRUNK)
+            prefix = prefix.replace(PREFIX_LEAF, PREFIX_EMP)
+
+        if self.has_child(node):
+            if node.right:
+                print(prefix + PREFIX_BRANCH + PREFIX_RIGHT + str(node.right))
+                if self.has_child(node.right):
+                    self._format_out(node.right, prefix + PREFIX_BRANCH)
+            else:
+                print(prefix + PREFIX_BRANCH + PREFIX_RIGHT)
+
+            if node.left:
+                print(prefix + PREFIX_LEAF + PREFIX_LEFT + str(node.left))
+                if self.has_child(node.left):
+                    self._format_out(node.left, prefix + PREFIX_LEAF)
+            else:
+                print(prefix + PREFIX_LEAF + PREFIX_LEFT)
+
+
+class CartesianTree(BinaryTree):
+    """笛卡尔树
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    def __init__(self, root=None, items=None) -> None:
+        """初始化二叉树
+
+        Keyword arguments:
+        root -- 根结点
+        items -- 数组
+        """
+        super().__init__(root)
+        if items:
+            self.build(items)
+
+    def build(self, items: list) -> bool:
+        """根结数组构建笛卡尔树
+
+        Keyword arguments:
+        items -- 数组
+        Return: 构建结果
+        """
+        for item in items:
+            node = BiTNode(item)
+
+            if not self.root:
+                self.root = node
+                continue
+
+            if item < self.root.element:
+                node.left, self.root = self.root, node
+            else:
+                if not self.root.right:
+                    self.root.right = node
+                else:
+                    pre_node = self.root
+                    current_node = self.root.right
+                    while current_node:
+                        if pre_node.element < item and item < current_node.element:
+                            break
+                        pre_node = current_node
+                        current_node = current_node.right
+                    pre_node.right = node
+                    node.left = current_node
+
+
+class BinarySearchTree(BinaryTree):
+    """二叉搜索树
+
+    满足以下条件
+        1. 若任意节点的左子树不空，则左子树上所有节点的值均小于它的根节点的值；
+        2. 若任意节点的右子树不空，则右子树上所有节点的值均大于或等于它的根节点的值；
+        3. 任意节点的左、右子树也分别为二叉查找树；
+
+    """
+
+    def __init__(self, root=None) -> None:
+        """初始化二叉树
+
+        Keyword arguments:
+        root -- 根节点
+        """
+
+        self.root = root
+
+    def put(self, element):
+        """插入节点
+
+        Keyword arguments:
+        element -- 节点值
+        """
+        node = BiTNode(element)
+        if not self.root:
+            self.root = node
+        else:
+            current = self.root
+            while current:
+                if node < current:
+                    if not current.left:
+                        current.left = node
+                        break
+                    else:
+                        current = current.left
+                else:
+                    if not current.right:
+                        current.right = node
+                        break
+                    else:
+                        current = current.right
+
+    def get(self, element, node=None) -> BiTNode:
+        """查找第一个匹配元素的节点
+
+        Keyword arguments:
+        element -- 匹配的元素
+        node -- 开始的节点
+        Return: 节点
+        """
+        current = node
+        if not current:
+            current = self.root
+
+        while current:
+            if current.element == element:
+                return current
+            elif current.element > element:
+                current = current.left
+            else:
+                current = current.right
 
     def remove(self, node: BiTNode, key) -> BiTNode:
         """移除第一个匹配元素的节点
@@ -249,80 +361,38 @@ class BinarySearchTree:
             node = node.right
         return node.element
 
-    @staticmethod
-    def has_child(node):
-        """判断节点是否有子节点
-
-        Keyword arguments:
-        node -- 节点
-        Return: return_description
-        """
-        if node.right or node.left:
-            return True
-        return False
-
-    def format_out(self, node=None) -> str:
-        """格式化输出
-
-        Keyword arguments:
-        node -- 节点
-        Return: return_description
-        """
-        if not node:
-            node = self.root
-
-        print(node)
-        self._format_out(node)
-
-    def _format_out(self, node, prefix=None):
-        if not prefix:
-            prefix = ''
-        else:
-            prefix = prefix.replace(PREFIX_BRANCH, PREFIX_TRUNK)
-            prefix = prefix.replace(PREFIX_LEAF, PREFIX_EMP)
-
-        if self.has_child(node):
-            if node.right:
-                print(prefix + PREFIX_BRANCH + PREFIX_RIGHT + str(node.right))
-                if self.has_child(node.right):
-                    self._format_out(node.right, prefix + PREFIX_BRANCH)
-            else:
-                print(prefix + PREFIX_BRANCH + PREFIX_RIGHT)
-
-            if node.left:
-                print(prefix + PREFIX_LEAF + PREFIX_LEFT + str(node.left))
-                if self.has_child(node.left):
-                    self._format_out(node.left, prefix + PREFIX_LEAF)
-            else:
-                print(prefix + PREFIX_LEAF + PREFIX_LEFT)
-
 
 def main():
-    tree = BinarySearchTree()
-    tree.put(5)
-    tree.put(3)
-    tree.put(4)
-    tree.put(2)
-    tree.put(1)
-    tree.put(7)
-    tree.put(6)
-    tree.put(8)
-    tree.put(9)
+    # tree = BinarySearchTree()
+    # tree.put(5)
+    # tree.put(3)
+    # tree.put(4)
+    # tree.put(2)
+    # tree.put(1)
+    # tree.put(7)
+    # tree.put(6)
+    # tree.put(8)
+    # tree.put(9)
 
+    # tree.format_out()
+
+    # tree.remove(tree.root, 1)
+
+    # tree.format_out()
+
+    # tree.remove(tree.root, 5)
+
+    # tree.format_out()
+
+    # tree.remove(tree.root, 10)
+
+    # tree.format_out()
+
+    # print(tree.in_order(tree.root))
+
+    items = [9, 3, 4, 5, 6, 7, 1, 2, 8, 10]
+    tree = CartesianTree(items=items)
     tree.format_out()
-
-    tree.remove(tree.root, 1)
-
-    tree.format_out()
-
-    tree.remove(tree.root, 5)
-
-    tree.format_out()
-
-    tree.remove(tree.root, 10)
-
-    tree.format_out()
-
     print(tree.in_order(tree.root))
 
 
