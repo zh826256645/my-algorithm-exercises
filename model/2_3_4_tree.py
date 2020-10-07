775727673c3339f3824bd7a4a406d5353a404ec6
钟浩 <zh826256645@gmail.com>
2020-10-07 17:35:43 +0800

    feat(二叉树): 新增 2-3 树 和 2-3-4 树

---
+++ model/2_3_4_tree.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+2-3 树 和 2-3-4 树
+
+@File    :   2_3_4_tree.py
+@Time    :   2020/09/24 11:33:49
+@Author  :   Zhong Hao
+@Version :   1.0
+@Contact :   zh826256645@gmail.com
+@Desc    :   None
+"""
+from model.binary_tree import BinarySearchTree, BiTNode
+
+
+class TwoThreeNode(BiTNode):
+    """2-3 树的节点
+
+    """
+    pass
+
+
+class TwoThreeTree(BinarySearchTree):
+    """2-3 树
+
+    """
+    pass
+
+
+class TwoThreeFourTree(BinarySearchTree):
+    """2-3-4 树
+
+    """
+    pass
